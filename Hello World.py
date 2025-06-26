aee35b3c76108bc1517ffce82d82472d9ebe2a4f
CyberYeti <yitmengchin@gmail.com>
2025-06-25 21:14:33 -0700

    added median function

--- Hello World.py
+++ Hello World.py
@@ -1,1 +1,17 @@
 print("goodbye World")
+
+def median(inp):
+    l = len(inp)
+    i1 = l//2
+    i2 = i1 - 1
+
+    if l%2 == 0:
+        return (inp[i1] + inp[i2])/2
+    else:
+        return inp[i1]
+    
+arr = [1,2,3,4,5]
+arr2 = [1,2,3,4]
+
+print(median(arr))
+print(median(arr2))
